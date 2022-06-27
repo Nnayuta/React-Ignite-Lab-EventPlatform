b58fab352d6f233e58eaecaa1915b98ff57c390e
Nayuta <naayudan@gmail.com>
2022-06-26 22:17:12 -0300

    Redirecionar para a primeira aula

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Event } from "./pages/Event";
 import { Subscribe } from "./pages/Subscribe";
 
+import { useGetLessonsQuery } from "./graphql/generated";
+
 export function Router() {
+
+    const { data } = useGetLessonsQuery();
+
     return (
         <Routes>
             <Route path="/" element={<Subscribe />} />
-            <Route path="/event" element={<Event />} />
-            <Route path="/event/lesson" element={<Event />} />
+            <Route path="/event" element={data && <Navigate replace to={`/event/lesson/${data?.lessons[0]?.slug}`} />} />
+            <Route path="/event/lesson" element={data && <Navigate replace to={`/event/lesson/${data?.lessons[0]?.slug}`} />} />
             <Route path="/event/lesson/:slug" element={<Event />} />
         </Routes>
     )
